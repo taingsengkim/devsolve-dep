b361c53e0948aaca8a9144186ec0715a9f5c6498
san tol <tolsan42@gmail.com>
2026-07-31 15:20:39 +0700

    Build bookmark page

--- src/lib/redux/services/baseApi.ts
+++ src/lib/redux/services/baseApi.ts
@@ -21,6 +21,6 @@ export const baseApi = createApi({
       return headers;
     },
   }),
-  tagTypes: ["User", "Post", "Report", "Program", "CompanyVerification", "ContentReport", "ModerationItem", "AdminUser", "Notification"],
+  tagTypes: ["User", "Post", "Report", "Program", "CompanyVerification", "ContentReport", "ModerationItem", "AdminUser", "Notification", "Bookmark"],
   endpoints: () => ({}),
 });
