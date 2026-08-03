deeb4d723af51399f32eec7bb4181b411934e9a0
san tol <tolsan42@gmail.com>
2026-08-03 14:32:23 +0700

    Fix erro.

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export default function PublicLayout({
   children,
@@ -15,4 +14,3 @@ export default function PublicLayout({
     </div>
   );
 }
-
